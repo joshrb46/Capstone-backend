9c8a0750e9227e1381f94b9d991ac45bca5216f9
joshrb46 <joshrb46@gmail.com>
2026-06-26 07:52:18 -0700

    updated to mount the HTTP server for Socket.IO

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const app = express();
 export default app;
 
 import usersRouter from "#api/users";
-import getUserFromToken from "#middleware/getUserFromToken";
+import lobbyRouter from "#api/lobby";
+import matchesRouter from "#api/matches";
+import roundsRouter from "#api/rounds";
+import wordsRouter from "#api/words";
+import getUserFromHeader from "#middleware/getUserFromHeader";
 import handlePostgresErrors from "#middleware/handlePostgresErrors";
 import cors from "cors";
 import morgan from "morgan";
@@ -15,11 +19,15 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(getUserFromToken);
+app.use(getUserFromHeader);
 
 app.get("/", (req, res) => res.send("Hello, World!"));
 
 app.use("/users", usersRouter);
+app.use("/lobby", lobbyRouter);
+app.use("/matches", matchesRouter);
+app.use("/rounds", roundsRouter);
+app.use("/words", wordsRouter);
 
 app.use(handlePostgresErrors);
 app.use((err, req, res, next) => {
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
+import http from "node:http";
 import app from "#app";
 import db from "#db/client";
+import { initSocket } from "#socket";
 
 const PORT = process.env.PORT ?? 3000;
 
 await db.connect();
 
-app.listen(PORT, () => {
+const httpServer = http.createServer(app);
+initSocket(httpServer);
+
+httpServer.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
 });
